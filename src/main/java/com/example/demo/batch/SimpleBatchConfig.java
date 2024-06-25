e2504b03267d600cdb918088c4986c2415e6e7ab
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2024-06-25 12:54:26 +0900

    fixed somelines for batch config.

--- src/main/java/com/example/demo/batch/SimpleBatchConfig.java
+++ src/main/java/com/example/demo/batch/SimpleBatchConfig.java
@@ -3,16 +3,14 @@ package com.example.demo.batch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.time.LocalDateTime;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,6 +22,7 @@ public class SimpleBatchConfig {
         return new JobBuilder("myJob", jobRepository)
                 .start(helloStep(jobRepository, platformTransactionManager))
                 .next(worldStep(jobRepository, platformTransactionManager))
+                .next(timeStep(jobRepository, platformTransactionManager))
                 .build();
     }
 
@@ -39,7 +38,15 @@ public class SimpleBatchConfig {
     public Step worldStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
         System.out.println("worldStep メソッドを実行");
         return new StepBuilder("myWorldStep", jobRepository)
-                .tasklet(new SimpleMessageTasklet("world"), platformTransactionManager)
+                .tasklet(new SimpleMessageTasklet("world!"), platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step timeStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        System.out.println("timeStep メソッドを実行");
+        return new StepBuilder("myTimeStep", jobRepository)
+                .tasklet(new SimpleMessageTasklet("Now is a timeStep."), platformTransactionManager)
                 .build();
     }
 }
