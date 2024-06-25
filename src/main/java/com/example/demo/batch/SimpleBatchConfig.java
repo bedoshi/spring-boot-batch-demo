cf1a03998fd4df4800da1d308f92ae9529f1e5b0
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2024-06-25 12:25:22 +0900

    added some files for batch.

---
+++ src/main/java/com/example/demo/batch/SimpleBatchConfig.java
@@ -0,0 +1,46 @@
+package com.example.demo.batch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableBatchProcessing
+public class SimpleBatchConfig {
+
+    @Bean
+    public Job myjob(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        System.out.println("myjob メソッドを実行");
+        return new JobBuilder("myjob", jobRepository)
+                .start(helloStep(jobRepository, platformTransactionManager))
+                .next(worldStep(jobRepository, platformTransactionManager))
+                .build();
+    }
+
+    @Bean
+    public Step helloStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        System.out.println("helloStep メソッドを実行");
+        return new StepBuilder("myHelloStep", jobRepository)
+                .tasklet(new SimpleMessageTasklet("Hello!"), platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step worldStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        System.out.println("worldStep メソッドを実行");
+        return new StepBuilder("myWorldStep", jobRepository)
+                .tasklet(new SimpleMessageTasklet("world"), platformTransactionManager)
+                .build();
+    }
+}
